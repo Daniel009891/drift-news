9b83b9387c407513493b1018a06f35f9499c2d78
Daniel009891 <>
2023-12-18 20:15:36 +0000

    Add article class to models.py

--- news/models.py
+++ news/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_article")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    votes_total = models.ManyToManyField(User, related_name='votes', blank=True)
+    
+
+
+    class Meta:
+        ordering = ['-votes']
+
+
+    def __str__(self):
+        return self.title
+
+
+    def number_of_votes(self):
+        return self.likes.count()
