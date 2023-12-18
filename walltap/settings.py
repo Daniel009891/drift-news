1ad871c18cd025ed4fcaadc404062d972414044b
Daniel009891 <>
2023-12-18 18:55:06 +0000

    fixing issues during first deployment

--- walltap/settings.py
+++ walltap/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['walltap-229162c12b90.herokuapp.com',
-                 '8000-daniel009891-drift-news-93xdimw.us2.codeanyapp.com'xgp, 'localhost']
+                 '8000-daniel009891-drift-news-93xdimw.us2.codeanyapp.com', 'localhost']
 
 
 # Application definition
