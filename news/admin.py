dc34ab90766c56ebdc3bd1bd950054bd99879375
Daniel009891 <>
2023-12-20 16:13:33 +0000

    Add article and comment filters to admin site

--- news/admin.py
+++ news/admin.py
@@ -1,10 +1,32 @@
-from django.contrib import admin
-from .models import Article
+from django.contrib import admin, messages
+from .models import Article, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(Article)
-class ArticleAdmin(SummernoteModelAdmin)
 
+@admin.register(Article)
+class ArticleAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug' : ('title',)}
+    list_filter = ('status', 'created_on', 'votes_total')
+    list_display = ('title', 'slug', 'status', 'created_on', 'author')
+    search_fields = ['title', 'content']
     summernote_fields = ('content')
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    
+    list_filter = ('approved', 'created_on')
+    list_display = ('name', 'body', 'article', 'created_on', 'approved')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    @admin.action(description='Mark selected comment(s) as approved?')
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+        
+        
+
+    
--- news/views.py
+++ news/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Article
 
 # Create your views here.
